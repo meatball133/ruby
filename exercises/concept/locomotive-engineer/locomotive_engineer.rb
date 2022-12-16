a5337b11217ae74048df1caac6b335eae7f9bc15
meatball <69751659+meatball133@users.noreply.github.com>
2022-12-16 23:59:30 +0100

    fix

--- exercises/concept/locomotive-engineer/locomotive_engineer.rb
+++ exercises/concept/locomotive-engineer/locomotive_engineer.rb
@@ -1,21 +1,21 @@
 class LocomotiveEngineer
-  def get_list_of_wagons():
+  def self.get_list_of_wagons():
     raise 'Please implement the LogLineParser#get_list_of_wagons method'
   end
 
-  def fix_list_of_wagons(each_wagons_id, missing_wagons):
+  def self.fix_list_of_wagons(each_wagons_id, missing_wagons):
     raise 'Please implement the LogLineParser#fix_list_of_wagons method'
   end
 
-  def add_missing_stops():
+  def self.add_missing_stops():
     raise 'Please implement the LogLineParser#add_missing_stops method'
   end
 
-  def extend_route_information(route, more_route_information):
+  def self.extend_route_information(route, more_route_information):
     raise 'Please implement the LogLineParser#extend_route_information method'
   end
 
-  def fix_wagon_depot(wagons_rows):
+  def self.fix_wagon_depot(wagons_rows):
     raise 'Please implement the LogLineParser#fix_wagon_depot method'
   end
 end
